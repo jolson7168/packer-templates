4e45fa7ecd65319f5feb65668fad761f5a177e8b
Nick Chappell <nick@intronic.org>
2013-09-29 16:37:44 -0700

    Removed git from the package list since it's going to be compiled from source in another script.

--- ubuntu-13.04-server-amd64/scripts/apt.sh
+++ ubuntu-13.04-server-amd64/scripts/apt.sh
@@ -4,4 +4,4 @@ apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install dkms
 apt-get -y install nfs-common
-apt-get -y install tmux emacs nano curl git vim tree htop smartmontools zsh mosh build-essential inotify-tools zerofree software-properties-common
+apt-get -y install tmux emacs nano curl vim tree htop smartmontools zsh mosh build-essential inotify-tools zerofree software-properties-common
