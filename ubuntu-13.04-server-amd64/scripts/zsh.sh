54f311312b5c77d2572a77898a93bccb00b1fb43
Nick Chappell <nick@intronic.org>
2013-09-29 17:07:53 -0700

    A non-hard-coded path should make the chsh a little more error-proof.

--- ubuntu-13.04-server-amd64/scripts/zsh.sh
+++ ubuntu-13.04-server-amd64/scripts/zsh.sh
@@ -7,8 +7,8 @@ sudo make
 sudo make install
 
 #Change the login shell to zsh for the vagrant and root users
-chsh -s /usr/bin/zsh vagrant
-chsh -s /usr/bin/zsh root
+chsh -s `which zsh` vagrant
+chsh -s `which zsh` root
 
 #Remove the tarball and tarball extract so they don't take up space in the final, packaged VM
 rm -rf /tmp/zsh-5.0.2/ /tmp/zsh-5.0.2.tar.gz
