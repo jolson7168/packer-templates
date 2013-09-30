0eec450a9d8d7c0c12680990b70c4af499c21a26
Nick Chappell <nick@intronic.org>
2013-09-29 21:56:39 -0700

    Added zshrc and oh my zsh theme installation for the root user; changed some relative paths to absolute ones.

--- ubuntu-13.04-server-amd64/scripts/root_zsh.sh
+++ ubuntu-13.04-server-amd64/scripts/root_zsh.sh
@@ -2,11 +2,24 @@
 su - root -c 'echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /root/.zshenv'
 
 #install oh-my-zsh first
-su - root -c 'curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+su - root -c 'cd /root/; curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
 #Make a folder for plugins like...
-su - root -c 'mkdir -p ~/.oh-my-zsh/custom/plugins'
-su - root -c 'cd ~/.oh-my-zsh/custom/plugins'
+su - root -c 'mkdir -p /root/.oh-my-zsh/custom/plugins'
+su - root -c 'cd /root/.oh-my-zsh/custom/plugins'
 #smarter history search
-su - root -c 'git clone git://github.com/zsh-users/zsh-history-substring-search.git'
+su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-history-substring-search.git'
 #syntax highlighting
-su - root -c 'git clone git://github.com/zsh-users/zsh-syntax-highlighting.git'
+su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-syntax-highlighting.git'
+
+#cat the uploaded vagrant .zshrc into the /home/vagrant/.zshrc file
+su - root -c 'cat /tmp/root-zshrc > /root/.zshrc'
+
+#cat in the uploaded ZSH theme
+su - root -c 'cat /tmp/root-zsh-theme.zsh-theme > /root/.oh-my-zsh/themes/root-zsh-theme.zsh-theme'
+
+#Install Vim Janus
+
+#Install rbenv and some plugins
+su - root -c 'cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/rbenv.git .rbenv'
+su - root -c 'cd /root/.rbenv/plugins; mkdir -p /root/.rbenv/plugins'
+su - root -c 'cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git'
