68b2fc90466f7f56e5e9e9e84fff66c915c6a386
Nick Chappell <nick@intronic.org>
2013-10-02 22:20:37 -0700

    Added 'cat', since the script just sits there otherwise.

--- ubuntu-13.04-server-amd64/scripts/git.sh
+++ ubuntu-13.04-server-amd64/scripts/git.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 #Install some prerequisites for git from apt so we don't have to hunt them down manually
-sudo apt-get -y install build-essential gettext libcurl4-gnutls-dev libexpat1-dev
+apt-get -y install build-essential gettext libcurl4-gnutls-dev libexpat1-dev
 
 cd /tmp
 wget https://git-core.googlecode.com/files/git-1.8.4.tar.gz
@@ -8,14 +8,14 @@ tar zxvf git-1.8.4.tar.gz
 cd git-1.8.4/
 ./configure
 #Compile it and put it into /usr/local
-sudo make prefix=/usr/local all; sudo make prefix=/usr/local install
+make prefix=/usr/local all; make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
 rm -rf /tmp/git-1.8.4/ /tmp/git-1.8.4.tar.gz
 
-su - vagrant -c '/tmp/gitconfig > /home/vagrant/.gitconfig'
-su - vagrant -c '/tmp/gitignore_global > /home/vagrant/.gitignore_global'
+su - vagrant -c 'cat /tmp/gitconfig > /home/vagrant/.gitconfig'
+su - vagrant -c 'cat /tmp/gitignore_global > /home/vagrant/.gitignore_global'
 
-su - vagrant -c '/tmp/gitconfig > /root/.gitconfig'
-su - vagrant -c '/tmp/gitignore_global > /root/.gitignore_global'
+su - vagrant -c 'cat /tmp/gitconfig > /root/.gitconfig'
+su - vagrant -c 'cat /tmp/gitignore_global > /root/.gitignore_global'
