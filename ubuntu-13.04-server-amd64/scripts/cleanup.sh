0299090069e066c2af452d4a200e6e94881094d9
Nick Chappell <nick@intronic.org>
2013-10-03 22:16:41 -0700

    Do the echo into /etc/network/interfaces before the disk zeroing; stuff tends not to work after the disk is zeroed.

--- ubuntu-13.04-server-amd64/scripts/cleanup.sh
+++ ubuntu-13.04-server-amd64/scripts/cleanup.sh
@@ -13,11 +13,13 @@ apt-get -y clean
 apt-get -y autoclean
 apt-get -y autoremove
 
+echo "pre-up sleep 2" >> /etc/network/interfaces
+exit
+
 #zero out disk space. Replacing free space with 0s makes the drive more easily compressed
 cat /dev/zero > zero.fill;sync;sleep 1;sync;rm -f zero.fill
 
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
 
-echo "pre-up sleep 2" >> /etc/network/interfaces
-exit
+
