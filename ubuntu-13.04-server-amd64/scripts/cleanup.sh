2894b665af3cd53a559308de2a030d4c78194940
Nick Chappell <nick@intronic.org>
2013-10-05 21:25:29 -0700

    Comments are nice.

--- ubuntu-13.04-server-amd64/scripts/cleanup.sh
+++ ubuntu-13.04-server-amd64/scripts/cleanup.sh
@@ -1,39 +1,40 @@
 #!/bin/sh -x
-echo "cleaning up dhcp leases"
+echo "Cleaning up dhcp leases..."
 rm /var/lib/dhcp/*
 
-echo "cleaning up udev rules"
+echo "Cleaning up udev rules..."
 rm /etc/udev/rules.d/70-persistent-net.rules
 mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
 #Remove oh-my-zsh git repo histories to save space; keep the current code checkout. We want oh-my-zsh but not the whole git repo's history
-
+echo "Removing oh-my-zsh git repos..."
 rm -rf /home/vagrant/.oh-my-zsh/.git
 rm -rf /root/.oh-my-zsh/.git
 
 #Remove rbenv git repo histories to save space; keep the current code checkout. We want rbenv but not the whole git repo's history
-
+echo "Removing rbenv git repos..."
 rm -rf /home/vagrant/.rbenv/.git
 rm -rf /root/.rbenv/.git
 
 #apt cleanup
-echo "ECHO apt-get remove kernel headers"
+echo "Running apt-get remove kernel headers..."
 apt-get -y remove linux-headers-$(uname -r)
-echo "ECHO remove older kernel headers"
+echo "Running remove older kernel headers"
 dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
-echo "ECHO apt-get clean"
+echo "Running apt-get clean..."
 apt-get -y clean
-echo "ECHO apt-get autoclean"
+echo "Running apt-get autoclean..."
 apt-get -y autoclean
-echo "ECHO apt-get auto-remove"
+echo "Running apt-get auto-remove..."
 apt-get -y autoremove
 
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
 
 #zero out disk space. Replacing free space with 0s makes the drive more easily compressed
+ECHO "Zeroing out disk..."
 cat /dev/zero > zero.fill;sync;sleep 1;sync;rm -f zero.fill
 
 dd if=/dev/zero of=/EMPTY bs=1M
