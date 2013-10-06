858c5383e1de8e13185e372c3acb44e20fe4ed41
Nick Chappell <nick@intronic.org>
2013-10-06 00:03:14 -0700

    Removed su's to the vagrant user.

--- ubuntu-13.04-server-amd64/scripts/vagrant_zsh.sh
+++ ubuntu-13.04-server-amd64/scripts/vagrant_zsh.sh
@@ -1,19 +1,21 @@
 #!/bin/sh -x
 #Add this variable so keyboard stuff isn't wonky
-su - vagrant -c 'echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /home/vagrant/.zshenv'
+echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /home/vagrant/.zshenv
 
 #install oh-my-zsh first
-su - vagrant -c 'curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+cd /home/vagrant
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
 #Make a folder for plugins like...
-su - vagrant -c 'mkdir -p ~/.oh-my-zsh/custom/plugins'
-su - vagrant -c 'cd ~/.oh-my-zsh/custom/plugins'
+mkdir -p /home/vagrant/.oh-my-zsh/custom/plugins
+cd /home/vagrant/.oh-my-zsh/custom/plugins
 #smarter history search
-su - vagrant -c 'git clone git://github.com/zsh-users/zsh-history-substring-search.git'
+git clone git://github.com/zsh-users/zsh-history-substring-search.git
 #syntax highlighting
-su - vagrant -c 'git clone git://github.com/zsh-users/zsh-syntax-highlighting.git'
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 #cat the uploaded vagrant .zshrc into the /home/vagrant/.zshrc file
-su - vagrant -c 'cat /tmp/vagrant-zshrc > /home/vagrant/.zshrc'
+cat /tmp/vagrant-zshrc > /home/vagrant/.zshrc
 
 #cat in the uploaded ZSH theme
-su - vagrant -c 'cat /tmp/vagrant-zsh-theme.zsh-theme > /home/vagrant/.oh-my-zsh/themes/vagrant-zsh-theme.zsh-theme'
+cat /tmp/vagrant-zsh-theme.zsh-theme > /home/vagrant/.oh-my-zsh/themes/vagrant-zsh-theme.zsh-theme
