dfd9b8abb79fa829375e2a2cbc13940bae028459
Nick Chappell <nick@intronic.org>
2013-10-06 21:45:14 -0700

    Moved the disk zeroing into the cleanup script; modified the JSON template so the cleanup script is called in its own provisioner JSON object.

--- centos-6.4-amd64/scripts/cleanup.sh
+++ centos-6.4-amd64/scripts/cleanup.sh
@@ -6,3 +6,9 @@ rm -rf VBoxGuestAdditions_*.iso
 # Remove traces of mac address from network configuration
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
 rm /etc/udev/rules.d/70-persistent-net.rules
+
+# Zero out the free space to save space in the final image:
+cat /dev/zero > zero.fill;sync;sleep 1;sync;rm -f zero.fill
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
