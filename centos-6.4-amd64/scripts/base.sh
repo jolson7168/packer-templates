8e0d8945e4ab67075548e2f092cd80246b47c938
Nick Chappell <nick@intronic.org>
2013-10-06 21:32:49 -0700

    Added yum repo and OS updates.

--- centos-6.4-amd64/scripts/base.sh
+++ centos-6.4-amd64/scripts/base.sh
@@ -10,6 +10,10 @@ enabled=1
 gpgcheck=0
 EOM
 
+yum -y check-update
+
+yum -y update
+
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
 
 #more packages
