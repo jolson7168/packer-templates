f918c39706392cda8c034f7d1c3d8392c1774822
Nick Chappell <nick@intronic.org>
2013-10-06 21:49:27 -0700

    Remove git repo histories for oh-my-zsh and rbenv.

--- centos-6.4-amd64/scripts/cleanup.sh
+++ centos-6.4-amd64/scripts/cleanup.sh
@@ -7,6 +7,16 @@ rm -rf VBoxGuestAdditions_*.iso
 sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
 rm /etc/udev/rules.d/70-persistent-net.rules
 
+#Remove oh-my-zsh git repo histories to save space; keep the current code checkout. We want oh-my-zsh but not the whole git repo's history
+echo "Removing oh-my-zsh git repos..."
+rm -rf /home/vagrant/.oh-my-zsh/.git
+rm -rf /root/.oh-my-zsh/.git
+
+#Remove rbenv git repo histories to save space; keep the current code checkout. We want rbenv but not the whole git repo's history
+echo "Removing rbenv git repos..."
+rm -rf /home/vagrant/.rbenv/.git
+rm -rf /root/.rbenv/.git
+
 #Empty out log files, but don't delete them
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
 
