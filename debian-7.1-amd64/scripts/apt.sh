7fbcbc82cf4a7a7f3cfed4e32b807b98e507af34
Nick Chappell <nick@intronic.org>
2013-10-09 09:37:21 -0700

    Moved apt package installs to a new, separate apt.sh script.

---
+++ debian-7.1-amd64/scripts/apt.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -x
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install linux-headers-$(uname -r) build-essential
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+apt-get -y install dkms
+apt-get -y install nfs-common
+apt-get -y install --no-install-recommends rake
+apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common
