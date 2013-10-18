376900e2eddfc70106514c7bde4db5a4c779aef0
Nick Chappell <nick@intronic.org>
2013-10-18 12:49:07 -0700

    Remove old Ubuntu 13.04 template.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -15,9 +15,3 @@ packer build --only=virtualbox template.json
 #Ubuntu 13.10
 cd ../ubuntu-13.10-server-amd64
 packer build --only=virtualbox template.json
-
-#Old VM templates that we may still want freshly build VMs made from
-
-#Ubuntu 13.04
-cd ../old_templates/ubuntu-13.04-server-amd64
-packer build --only=virtualbox template.json
