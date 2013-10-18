6ddc69993f6b6785eb57d996bac7841a89cee45a
Nick Chappell <nick@intronic.org>
2013-10-18 12:48:52 -0700

    Added script to build old templates.

---
+++ old_virtualbox_vm_build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Go into the old_templates folder first; every other command will get run relative to this folder
+cd old_templates/
+
+#Debian 7.1
+cd ./debian-7.1-amd64
+packer build --only=virtualbox template.json
+
+#Ubuntu 12.10
+cd ../ubuntu-12.10-server-amd64
+packer build --only=virtualbox template.json
+
+#Ubuntu 13.04
+cd ../ubuntu-13.04-server-amd64
+packer build --only=virtualbox template.json
