e12aa78e02b19784dd649a4ffed03f58499b48ab
Nick Chappell <nick@intronic.org>
2013-10-18 12:49:24 -0700

    Comments.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -8,7 +8,8 @@ packer build --only=virtualbox template.json
 cd ../centos-6.4-amd64
 packer build --only=virtualbox template.json
 
-#Ubuntu 12.04
+#Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is the most recent LTS release
+#so we still want to have fresh builds of it
 cd ../ubuntu-12.04-server-amd64
 packer build --only=virtualbox template.json
 
