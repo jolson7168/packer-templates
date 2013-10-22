8c750469b008281ac86b59d1cb721f4e1cd6fe6a
Nick Chappell <nick@intronic.org>
2013-10-21 21:02:58 -0700

    Added scripts.

--- ubuntu-12.04-server-amd64/scripts/base.sh
+++ ubuntu-12.04-server-amd64/scripts/base.sh
@@ -1,13 +1,3 @@
-
-# Apt-install various things necessary for Ruby, guest additions,
-# etc., and remove optional things to trim down the machine.
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install gcc build-essential linux-headers-$(uname -r)
-apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
-apt-get -y install vim curl ncdu
-apt-get clean
-
 # Set up sudo
 ( cat <<'EOP'
 %vagrant ALL=NOPASSWD:ALL
