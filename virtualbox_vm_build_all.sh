69ea106188f42cac9831c960331c36d2dacf19ec
Nick Chappell <nick@intronic.org>
2013-10-25 16:00:18 -0700

    Add FreeBSD 9.1 build to the VirtualBox build-all script.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -16,3 +16,7 @@ packer build --only=virtualbox template.json
 #Ubuntu 13.10
 cd ../ubuntu-13.10-server-amd64
 packer build --only=virtualbox template.json
+
+#FreeBSD 9.1
+cd ../freebsd-9.1-amd64
+packer build --only=virtualbox template.json
