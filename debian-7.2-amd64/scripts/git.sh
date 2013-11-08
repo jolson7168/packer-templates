f1c69877568210e3d03357bc95d91cfbc7599319
Nick Chappell <nick@intronic.org>
2013-11-08 11:22:31 -0800

    Updated git version.

--- debian-7.2-amd64/scripts/git.sh
+++ debian-7.2-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 apt-get -y install build-essential gettext libcurl4-gnutls-dev libexpat1-dev
 
 cd /tmp
-wget https://git-core.googlecode.com/files/git-1.8.4.tar.gz
-tar zxvf git-1.8.4.tar.gz
-cd git-1.8.4/
+wget https://git-core.googlecode.com/files/git-1.8.4.2.tar.gz
+tar zxvf git-1.8.4.2.tar.gz
+cd git-1.8.4.2/
 ./configure
 #Compile it and put it into /usr/local
 make prefix=/usr/local all; make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-1.8.4/ /tmp/git-1.8.4.tar.gz
+rm -rf /tmp/git-1.8.4.2/ /tmp/git-1.8.4.2.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
