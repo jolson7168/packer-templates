a264018165e18d575923a6ed424a2bb87f566bf0
Nick Chappell <nick@intronic.org>
2013-11-11 12:16:12 -0800

    Update Git to version 1.8.4.3.

--- centos-6.4-amd64/scripts/git.sh
+++ centos-6.4-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 yum install -y gcc make gcc-c++ zlib-devel perl-ExtUtils-Embed gettext curl-devel zlib-devel openssl-devel cpio expat-devel gettext-devel
 
 cd /tmp
-wget https://git-core.googlecode.com/files/git-1.8.4.2.tar.gz
-tar zxvf git-1.8.4.2.tar.gz
-cd git-1.8.4.2/
+wget https://git-core.googlecode.com/files/git-1.8.4.3.tar.gz
+tar zxvf git-1.8.4.3.tar.gz
+cd git-1.8.4.3/
 ./configure
 #Compile it and put it into /usr/local
 sudo make prefix=/usr/local all; sudo make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-1.8.4.2/ /tmp/git-1.8.4.2.tar.gz
+rm -rf /tmp/git-1.8.4.3/ /tmp/git-1.8.4.3.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
