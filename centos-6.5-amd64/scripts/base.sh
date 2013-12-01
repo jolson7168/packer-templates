0138d1acfe4d43ea6b9d52fd4c27a294f1aa7703
Nick Chappell <nick@intronic.org>
2013-12-01 12:36:49 -0800

    Added CentOS 6.5 AMD64 template.

---
+++ centos-6.5-amd64/scripts/base.sh
@@ -0,0 +1,26 @@
+# Base install
+
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+cat > /etc/yum.repos.d/epel.repo << EOM
+[epel]
+name=epel
+baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y check-update
+
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+
+#disable iptables
+chkconfig iptables off
+service iptables stop
+
+#more packages
+yum -y install man nc vim nano emacs tmux htop tree lsof curl wget screen telnet mailx unzip yum-utils rsync ncdu mosh
+
+#Install ag
+rpm -Uvhi http://swiftsignal.com/packages/centos/6/x86_64/the-silver-searcher-0.14-1.el6.x86_64.rpm
+
---
+++ centos-6.5-amd64/scripts/cleanup.sh
@@ -0,0 +1,33 @@
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y clean all
+#rm -rf /etc/yum.repos.d/{puppetlabs,epel}.repo
+rm -rf VBoxGuestAdditions_*.iso
+
+# Remove traces of mac address from network configuration
+sed -i /HWADDR/d /etc/sysconfig/network-scripts/ifcfg-eth0
+rm /etc/udev/rules.d/70-persistent-net.rules
+
+#Remove oh-my-zsh git repo histories to save space; keep the current code checkout. We want oh-my-zsh but not the whole git repo's history
+echo "Removing oh-my-zsh git repos..."
+rm -rf /home/vagrant/.oh-my-zsh/.git
+rm -rf /root/.oh-my-zsh/.git
+
+#Remove rbenv git repo histories to save space; keep the current code checkout. We want rbenv but not the whole git repo's history
+echo "Removing rbenv git repos..."
+rm -rf /home/vagrant/.rbenv/.git
+rm -rf /root/.rbenv/.git
+
+#Remove non-English localization files
+sudo rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
+
+#Empty out log files, but don't delete them
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+#Clean out some other folders
+rm -rf /var/tmp/*
+
+# Zero out the free space to save space in the final image:
+cat /dev/zero > zero.fill;sync;sleep 1;sync;rm -f zero.fill
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
