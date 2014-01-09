cdf799c4216d7c31ba7ca219eaf64dd9a5d67509
Nick Chappell <nick@intronic.org>
2014-01-08 17:30:15 -0800

    Changed instances of 'virtualbox' to 'virtualbox-iso' to conform to Packer v0.5.0+ renaming the VirutalBox builder.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -2,17 +2,17 @@
 
 #Debian 7.3
 cd ./debian-7.3-amd64
-packer build --only=virtualbox template.json
+packer build --only=virtualbox-iso template.json
 
 #CentOS 6.5
 cd ../centos-6.5-amd64
-packer build --only=virtualbox template.json
+packer build --only=virtualbox-iso template.json
 
 #Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is the most recent LTS release
 #so we still want to have fresh builds of it
 cd ../ubuntu-12.04-server-amd64
-packer build --only=virtualbox template.json
+packer build --only=virtualbox-iso template.json
 
 #Ubuntu 13.10
 cd ../ubuntu-13.10-server-amd64
-packer build --only=virtualbox template.json
+packer build --only=virtualbox-iso template.json
