622cba39c915bf580bffd48a57c542ac9efc7d76
Nick Chappell <nick@intronic.org>
2014-02-09 23:21:59 -0800

    Added Debian 7.2 and 7.3 build commands to the old template build script.

--- old_virtualbox_vm_build_all.sh
+++ old_virtualbox_vm_build_all.sh
@@ -7,6 +7,14 @@ cd old_templates/
 cd ./debian-7.1-amd64
 packer build --only=virtualbox template.json
 
+#Debian 7.2
+cd ./debian-7.2-amd64
+packer build --only=virtualbox template.json
+
+#Debian 7.3
+cd ./debian-7.3-amd64
+packer build --only=virtualbox template.json
+
 #CentOS 6.4
 cd ../centos-6.4-amd64
 packer build --only=virtualbox template.json
