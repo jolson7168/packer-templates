cd216d7392e58e5e45897736a70a8b04a24921df
Nick Chappell <nick@intronic.org>
2014-02-11 11:34:49 -0800

    Added the metric tool packages to the Ubuntu 12.04 template.

--- ubuntu-12.04-server-amd64/scripts/apt.sh
+++ ubuntu-12.04-server-amd64/scripts/apt.sh
@@ -6,6 +6,9 @@ apt-get -y install nfs-common
 apt-get -y install --no-install-recommends rake
 apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common ncdu unzip
 
+#Some metrics gathering tools
+apt-get -y install apachetop atop nmon dstat sysstat glances goaccess iotop
+
 #Install ag
 cd /tmp
 wget http://swiftsignal.com/packages/ubuntu/precise/the-silver-searcher_0.14-1_amd64.deb
