c7d1a3b82dadaeb017e0b920011164c48b5e628b
Nick Chappell <nick@intronic.org>
2014-02-19 10:18:19 -0800

    Removed the su - commands since Packer runs these scripts as root on the VMs anyway.

--- centos-6.5-amd64/scripts/zsh.sh
+++ centos-6.5-amd64/scripts/zsh.sh
@@ -23,7 +23,7 @@ echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /home/vagrant/.zshenv
 
 #install oh-my-zsh first
 cd /home/vagrant
-su - vagrant -c 'curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 #Make a folder for plugins like...
 mkdir -p /home/vagrant/.oh-my-zsh/custom/plugins
@@ -39,10 +39,10 @@ cat /tmp/vagrant-zshrc > /home/vagrant/.zshrc
 cat /tmp/vagrant-zsh-theme.zsh-theme > /home/vagrant/.oh-my-zsh/themes/vagrant-zsh-theme.zsh-theme
 
 #Install rbenv and some plugins
-su - root -c 'git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv'
-su - root -c 'mkdir -p /home/vagrant/.rbenv/plugins'
-su - root -c 'cd /home/vagrant/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git'
-su - root -c 'chown -R vagrant:vagrant /home/vagrant/.rbenv/'
+git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+mkdir -p /home/vagrant/.rbenv/plugins
+cd /home/vagrant/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git
+chown -R vagrant:vagrant /home/vagrant/.rbenv/
 
 
 ##############################
@@ -50,28 +50,28 @@ su - root -c 'chown -R vagrant:vagrant /home/vagrant/.rbenv/'
 ##############################
 
 #Add this variable so keyboard stuff isn't wonky
-su - root -c 'echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /root/.zshenv'
+echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /root/.zshenv
 
 #install oh-my-zsh first
-su - root -c 'cd /root/; curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+cd /root/; curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 #Make a folder for plugins like...
-su - root -c 'mkdir -p /root/.oh-my-zsh/custom/plugins'
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins'
+mkdir -p /root/.oh-my-zsh/custom/plugins
+cd /root/.oh-my-zsh/custom/plugins
 #smarter history search
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-history-substring-search.git'
+cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-history-substring-search.git
 #syntax highlighting
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-syntax-highlighting.git'
+cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 #cat the uploaded vagrant .zshrc into the /home/vagrant/.zshrc file
-su - root -c 'cat /tmp/root-zshrc > /root/.zshrc'
+cat /tmp/root-zshrc > /root/.zshrc
 
 #cat in the uploaded ZSH theme
-su - root -c 'cat /tmp/root-zsh-theme.zsh-theme > /root/.oh-my-zsh/themes/root-zsh-theme.zsh-theme'
+cat /tmp/root-zsh-theme.zsh-theme > /root/.oh-my-zsh/themes/root-zsh-theme.zsh-theme
 
 #Install rbenv and some plugins
-su - root -c 'git clone git://github.com/sstephenson/rbenv.git /root/.rbenv'
-su - root -c 'mkdir -p /root/.rbenv/plugins'
-su - root -c 'cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git'
+git clone git://github.com/sstephenson/rbenv.git /root/.rbenv
+mkdir -p /root/.rbenv/plugins
+cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git
 
 #Change the login shell to zsh for the vagrant and root users
 echo "/usr/local/bin/zsh" >> /etc/shells
--- ubuntu-13.10-server-amd64/scripts/zsh.sh
+++ ubuntu-13.10-server-amd64/scripts/zsh.sh
@@ -48,25 +48,25 @@ cat /tmp/vagrant-zsh-theme.zsh-theme > /home/vagrant/.oh-my-zsh/themes/vagrant-z
 ##############################
 
 #Add this variable so keyboard stuff isn't wonky
-su - root -c 'echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /root/.zshenv'
+echo "DEBIAN_PREVENT_KEYBOARD_CHANGES=yes" > /root/.zshenv
 
 #install oh-my-zsh first
-su - root -c 'cd /root/; curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh'
+cd /root/; curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 #Make a folder for plugins like...
-su - root -c 'mkdir -p /root/.oh-my-zsh/custom/plugins'
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins'
+mkdir -p /root/.oh-my-zsh/custom/plugins'
+cd /root/.oh-my-zsh/custom/plugins'
 #smarter history search
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-history-substring-search.git'
+cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-history-substring-search.git
 #syntax highlighting
-su - root -c 'cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-syntax-highlighting.git'
+cd /root/.oh-my-zsh/custom/plugins; git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 #cat the uploaded vagrant .zshrc into the /home/vagrant/.zshrc file
-su - root -c 'cat /tmp/root-zshrc > /root/.zshrc'
+cat /tmp/root-zshrc > /root/.zshrc
 
 #cat in the uploaded ZSH theme
-su - root -c 'cat /tmp/root-zsh-theme.zsh-theme > /root/.oh-my-zsh/themes/root-zsh-theme.zsh-theme'
+cat /tmp/root-zsh-theme.zsh-theme > /root/.oh-my-zsh/themes/root-zsh-theme.zsh-theme
 
 #Install rbenv and some plugins
-su - root -c 'git clone git://github.com/sstephenson/rbenv.git /root/.rbenv'
-su - root -c 'mkdir -p /root/.rbenv/plugins'
-su - root -c 'cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git'
+git clone git://github.com/sstephenson/rbenv.git /root/.rbenv
+mkdir -p /root/.rbenv/plugins
+cd /root/.rbenv/plugins; git clone git://github.com/sstephenson/ruby-build.git
