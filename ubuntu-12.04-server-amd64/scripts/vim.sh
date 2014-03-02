a5a1f5e773c076713abe3bb1fc1f1ad7b51b215f
Nick Chappell <nick@intronic.org>
2014-03-02 15:33:21 -0800

    Added the su - commands back because the Janus script has to be run as the user we're setting it up for.

--- ubuntu-12.04-server-amd64/scripts/vim.sh
+++ ubuntu-12.04-server-amd64/scripts/vim.sh
@@ -2,13 +2,13 @@
 
 #Install Janus for the 'vagrant' user (cd to vagrant's homedir first so we're in the right spot for the script to run)
 echo "Installing Vim Janus for the 'vagrant' user..."
-cd /home/vagrant; curl -Lo- https://bit.ly/janus-bootstrap | bash
+su - vagrant -c 'cd /home/vagrant; curl -Lo- https://bit.ly/janus-bootstrap | bash'
 
 chown -R vagrant:vagrant /home/vagrant/
 
 #Install Janus for the 'root' user (cd to root's homedir first so we're in the right spot for the script to run)
 echo "Installing Vim Janus for the 'root' user..."
-cd /root; curl -Lo- https://bit.ly/janus-bootstrap | bash
+su - root -c 'cd /root; curl -Lo- https://bit.ly/janus-bootstrap | bash'
 
 #Remove the janus git repo history to save sapce. We want the current checkout of the files but not the whole repo history
 echo "Removing git repo histories for Janus to save space..."
