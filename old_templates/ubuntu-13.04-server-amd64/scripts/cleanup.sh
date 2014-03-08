e90a3ae8baec43c68ab9e8614e76a076d3680a3c
Nick Chappell <nick@intronic.org>
2014-03-08 13:03:32 -0800

    Get rid of non-English locale files by moving the en files out of the way, deleting the contents of /usr/share/locale, then moving the en files back into place. This is less fragile than trying to use shell globbing.

--- old_templates/ubuntu-13.04-server-amd64/scripts/cleanup.sh
+++ old_templates/ubuntu-13.04-server-amd64/scripts/cleanup.sh
@@ -34,7 +34,9 @@ apt-get -y autoremove
 
 #Remove non-English locale files
 echo "Removing non-English locale files"
-sudo rm -rf /usr/share/locale/{af,am,ar,as,ast,az,bal,be,bg,bn,bn_IN,br,bs,byn,ca,cr,cs,csb,cy,da,de,de_AT,dz,el,en_AU,en_CA,eo,es,et,et_EE,eu,fa,fi,fo,fr,fur,ga,gez,gl,gu,haw,he,hi,hr,hu,hy,id,is,it,ja,ka,kk,km,kn,ko,kok,ku,ky,lg,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,nb,ne,nl,nn,no,nso,oc,or,pa,pl,ps,pt,pt_BR,qu,ro,ru,rw,si,sk,sl,so,sq,sr,sr*latin,sv,sw,ta,te,th,ti,tig,tk,tl,tr,tt,ur,urd,ve,vi,wa,wal,wo,xh,zh,zh_HK,zh_CN,zh_TW,zu}
+mv /usr/share/locale/en /tmp
+rm -rf /usr/share/locale/*
+mv /tmp/en /usr/share/locale
 
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
