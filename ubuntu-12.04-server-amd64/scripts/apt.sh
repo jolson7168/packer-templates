02a4aad593cbba5dc57f5d59af6dec3505159591
Nick Chappell <nick@intronic.org>
2014-03-26 14:45:19 -0700

    Install the PipeViewer package: http://www.ivarch.com/programs/pv.shtml

--- ubuntu-12.04-server-amd64/scripts/apt.sh
+++ ubuntu-12.04-server-amd64/scripts/apt.sh
@@ -4,7 +4,7 @@ apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install dkms
 apt-get -y install nfs-common
 apt-get -y install --no-install-recommends rake
-apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common ncdu unzip
+apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common ncdu unzip pv
 
 #Some metrics gathering tools
 apt-get -y install apachetop atop nmon dstat sysstat glances goaccess iotop
