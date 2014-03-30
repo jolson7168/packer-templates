522fbfe23b79530725a35e448172cd2fc96891c4
Nick Chappell <nick@intronic.org>
2014-03-30 15:48:52 -0700

    Roll back to Git 1.9.0 because tarballs for 1.9.1 are not available.

--- old_templates/centos-6.4-amd64/scripts/git.sh
+++ old_templates/centos-6.4-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 yum install -y gcc make gcc-c++ zlib-devel perl-ExtUtils-Embed gettext curl-devel zlib-devel openssl-devel cpio expat-devel gettext-devel
 
 cd /tmp
-wget https://git-core.googlecode.com/files/git-1.9.1.tar.gz
-tar zxvf git-1.9.1.tar.gz
-cd git-1.9.1/
+wget https://git-core.googlecode.com/files/git-1.9.0.tar.gz
+tar zxvf git-1.9.0.tar.gz
+cd git-1.9.0/
 ./configure
 #Compile it and put it into /usr/local
 sudo make prefix=/usr/local all; sudo make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-1.9.1/ /tmp/git-1.9.1.tar.gz
+rm -rf /tmp/git-1.9.0/ /tmp/git-1.9.0.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
