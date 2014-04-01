9d00a19da263a67ce629374a1905e03427682278
Nick Chappell <nick@intronic.org>
2014-03-31 22:07:29 -0700

    Add some missing packages.

--- works_in_progress/ubuntu-14.04-server-amd64/scripts/apt.sh
+++ works_in_progress/ubuntu-14.04-server-amd64/scripts/apt.sh
@@ -4,6 +4,6 @@ apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
 apt-get -y install dkms
 apt-get -y install nfs-common
 apt-get -y install --no-install-recommends rake
-apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common ncdu silversearcher-ag unzip
+apt-get -y install tmux emacs nano curl vim tree htop smartmontools mosh inotify-tools zerofree software-properties-common ncdu silversearcher-ag unzip ccze pv
 #Some metrics gathering tools
 apt-get -y install apachetop atop nmon dstat sysstat glances goaccess iotop
