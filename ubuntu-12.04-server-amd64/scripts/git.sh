61f82705a95f1a9d93890305ba249d4d8e2047fc
Nick Chappell <nick@intronic.org>
2014-05-31 22:17:39 -0700

    Upgrade to Git 1.9.4 and grab the tarball from kernel.org.

--- ubuntu-12.04-server-amd64/scripts/git.sh
+++ ubuntu-12.04-server-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 apt-get -y install build-essential gettext libcurl4-gnutls-dev libexpat1-dev
 
 cd /tmp
-wget https://git-core.googlecode.com/files/git-1.9.0.tar.gz
-tar zxvf git-1.9.0.tar.gz
-cd git-1.9.0/
+wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz
+tar zxvf git-1.9.4.tar.gz
+cd git-1.9.4/
 ./configure
 #Compile it and put it into /usr/local
 make prefix=/usr/local all; make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-1.9.0/ /tmp/git-1.9.0.tar.gz
+rm -rf /tmp/git-1.9.4/ /tmp/git-1.9.4.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
