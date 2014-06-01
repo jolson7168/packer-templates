9471ae997f30719605481d0969499701753a776f
Nick Chappell <nick@intronic.org>
2014-05-31 22:27:05 -0700

    Language change; 14.04 is now the most recent LTS release.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -8,7 +8,7 @@ packer build --only=virtualbox-iso template.json
 cd ../centos-6.5-amd64
 packer build --only=virtualbox-iso template.json
 
-#Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is the most recent LTS release
+#Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is a still-supported LTS release
 #so we still want to have fresh builds of it
 cd ../ubuntu-12.04-server-amd64
 packer build --only=virtualbox-iso template.json
