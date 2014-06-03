d8fe7435e1ead0c347df5aca7e5bfc6856a9c91d
Nick Chappell <nick@intronic.org>
2014-06-03 09:03:39 -0700

    Install Git 2.0.0 instead of 1.9.4.

--- ubuntu-14.04-server-amd64/scripts/git.sh
+++ ubuntu-14.04-server-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 apt-get -y install build-essential gettext libcurl4-gnutls-dev libexpat1-dev
 
 cd /tmp
-wget https://www.kernel.org/pub/software/scm/git/git-1.9.4.tar.gz
-tar zxvf git-1.9.4.tar.gz
-cd git-1.9.4/
+wget https://www.kernel.org/pub/software/scm/git/git-2.0.0.tar.gz
+tar zxvf git-2.0.0.tar.gz
+cd git-2.0.0/
 ./configure
 #Compile it and put it into /usr/local
 make prefix=/usr/local all; make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-1.9.4/ /tmp/git-1.9.4.tar.gz
+rm -rf /tmp/git-2.0.0/ /tmp/git-2.0.0.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
