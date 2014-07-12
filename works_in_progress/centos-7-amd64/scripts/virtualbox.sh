90bd0ff50c34c85cc691e4c737fa17ff4b65ea1c
Nick Chappell <nick@intronic.org>
2014-07-12 16:12:46 -0700

    Hard code a newer guest additions ISO file name; this is to get around the guest additions not compiling properly.

--- works_in_progress/centos-7-amd64/scripts/virtualbox.sh
+++ works_in_progress/centos-7-amd64/scripts/virtualbox.sh
@@ -1,7 +1,7 @@
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/veewee/.vbox_version)
 cd /tmp
-mount -o loop /home/veewee/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+mount -o loop /home/veewee/VBoxGuestAdditions_4.3.13-94832.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/veewee/VBoxGuestAdditions_*.iso
