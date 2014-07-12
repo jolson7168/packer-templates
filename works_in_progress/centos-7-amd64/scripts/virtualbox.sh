55f25d3ada2fd76ac9dd43e35fd2c9ccb6b42fc5
Nick Chappell <nick@intronic.org>
2014-07-12 16:54:23 -0700

    Try listing what's in veewee's homedir so we can see if the guest additions ISO is there with the correct name before it's mounted.

--- works_in_progress/centos-7-amd64/scripts/virtualbox.sh
+++ works_in_progress/centos-7-amd64/scripts/virtualbox.sh
@@ -1,6 +1,8 @@
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/veewee/.vbox_version)
 cd /tmp
+echo "veewee homedir"
+ls -la /home/veewee
 mount -o loop /home/veewee/VBoxGuestAdditions_4.3.13-94832.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
