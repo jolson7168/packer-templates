11adfa4f7e8490bfd68fb14aadb01a508d6be202
Nick Chappell <nick@intronic.org>
2014-07-12 13:53:23 -0700

    Use systemctl commands instead of service and chkconfig to disable the firewall daemons.

--- works_in_progress/centos-7-amd64/scripts/base.sh
+++ works_in_progress/centos-7-amd64/scripts/base.sh
@@ -2,9 +2,14 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-#disable iptables
-chkconfig iptables off
-service iptables stop
+#Stop and disable iptables
+systemctl stop iptables
+systemctl disable iptables
+
+#Stop and disable firewalld
+systemctl stop firewalld
+systemctl disable firewalld
+
 
 #Don't do reverse DNS lookups of SSH clients that connect; this usually speeds up SSH by quite a bit:
 echo "UseDNS no" >> /etc/ssh/sshd_config
