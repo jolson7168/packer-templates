efd3af4c44fde99c23b43a83d59847b5b75f1fbb
Nick Chappell <nick@intronic.org>
2014-07-13 14:20:40 -0700

    Use the right file path when removing the guest additions ISO.

--- works_in_progress/centos-7-amd64/scripts/virtualbox.sh
+++ works_in_progress/centos-7-amd64/scripts/virtualbox.sh
@@ -1,10 +1,10 @@
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/veewee/.vbox_version)
 cd /tmp
-echo "veewee homedir"
+echo "veewee homedir's contents:"
 ls -la /home/veewee
 mount -o loop /home/veewee/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm -rf /home/veewee/VBoxGuestAdditions_*.iso
-
+#Remove the guest additions ISO so it doesn't take up space in the finished base box:
+rm -rf /home/veewee/VBoxGuestAdditions.iso
