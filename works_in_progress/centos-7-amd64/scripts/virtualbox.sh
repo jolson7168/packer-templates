c85eac7057f9214cfc17a1ceccd85a86cc9118f8
Nick Chappell <nick@intronic.org>
2014-07-13 14:16:01 -0700

    Use the correct path for the guest additions ISO.

--- works_in_progress/centos-7-amd64/scripts/virtualbox.sh
+++ works_in_progress/centos-7-amd64/scripts/virtualbox.sh
@@ -3,7 +3,7 @@ VBOX_VERSION=$(cat /home/veewee/.vbox_version)
 cd /tmp
 echo "veewee homedir"
 ls -la /home/veewee
-mount -o loop /home/veewee/VBoxGuestAdditions_4.3.13-94832.iso /mnt
+mount -o loop /home/veewee/VBoxGuestAdditions.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 rm -rf /home/veewee/VBoxGuestAdditions_*.iso
