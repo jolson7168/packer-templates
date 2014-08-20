50a3b24bf86590437fcc3b0c200a6e7675501770
Nick Chappell <nick@intronic.org>
2014-08-19 20:32:03 -0700

    Install Git version 2.1.0.

--- works_in_progress/rhel-7-amd64/scripts/git.sh
+++ works_in_progress/rhel-7-amd64/scripts/git.sh
@@ -3,16 +3,16 @@
 yum install -y gcc make gcc-c++ zlib-devel perl-ExtUtils-Embed gettext curl-devel zlib-devel openssl-devel cpio expat-devel gettext-devel
 
 cd /tmp
-wget https://www.kernel.org/pub/software/scm/git/git-2.0.1.tar.gz
-tar zxvf git-2.0.1.tar.gz
-cd git-2.0.1/
+wget https://www.kernel.org/pub/software/scm/git/git-2.1.0.tar.gz
+tar zxvf git-2.1.0.tar.gz
+cd git-2.1.0/
 ./configure
 #Compile it and put it into /usr/local
 sudo make prefix=/usr/local all; sudo make prefix=/usr/local install
 
 #remove the source code and tarball so they don't take up space in the final, packaged VM
 cd /tmp
-rm -rf /tmp/git-2.0.1/ /tmp/git-2.0.1.tar.gz
+rm -rf /tmp/git-2.1.0/ /tmp/git-2.1.0.tar.gz
 
 cp /tmp/gitconfig /home/vagrant/.gitconfig
 cp /tmp/gitignore_global /home/vagrant/.gitignore_global
