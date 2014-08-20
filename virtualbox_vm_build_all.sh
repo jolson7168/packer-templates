0172cadaf6672651a30e15ef02c81018d01828fe
Nick Chappell <nick@intronic.org>
2014-08-19 23:17:40 -0700

    Added an entry for the CentOS 7.0 template in virtualbox_vm_build_all.sh.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -8,6 +8,10 @@ packer build --only=virtualbox-iso template.json
 cd ../centos-6.5-amd64
 packer build --only=virtualbox-iso template.json
 
+#CentOS 7.0
+cd ../centos-7.0-amd64
+packer build --only=virtualbox-iso template.json
+
 #Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is a still-supported LTS release
 #so we still want to have fresh builds of it
 cd ../ubuntu-12.04-server-amd64
