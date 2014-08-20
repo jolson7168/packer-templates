2494b3f81879ecdb7151d4d7ae6d3facca990490
Nick Chappell <nick@intronic.org>
2014-08-19 22:05:48 -0700

    Add a yum install command to install prereqs for the guest additions inside of the virtualbox.sh script in the CentOS 6.5 template. This is in case people don't include the yum.sh script in their build; this makes the virtualbox.sh script more standalone.

--- centos-6.5-amd64/scripts/virtualbox.sh
+++ centos-6.5-amd64/scripts/virtualbox.sh
@@ -1,3 +1,6 @@
+#Install kernel headers, GCC and some other things that we'll need to compile and install the guest additions:
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+
 # Installing the virtualbox guest additions
 VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
 cd /tmp
