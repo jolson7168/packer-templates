21251f8d40e6534f509fe7449d5a5f265981a84c
Nick Chappell <nick@intronic.org>
2014-09-20 15:13:19 -0700

    Change the shells for the vagrant and root users to bash.

--- works_in_progress/freebsd-10.0-amd64/scripts/bash.sh
+++ works_in_progress/freebsd-10.0-amd64/scripts/bash.sh
@@ -1,1 +1,9 @@
 #Install and configure the bash shell
+
+#bash is already installed
+
+#Change the default shell for the vagrant and root users to bash
+#Source: https://www.freebsd.org/doc/handbook/shells.html
+
+chsh -s /usr/local/bin/bash vagrant
+chsh -s /usr/local/bin/bash root
