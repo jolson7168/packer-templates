6735665dcc889d53e0e7c4da0fb073eb09c7d54e
Nick Chappell <nick@intronic.org>
2014-09-20 16:39:28 -0700

    Added a note about where the shell changing commands came from.

--- works_in_progress/freebsd-10.0-amd64/scripts/bash.sh
+++ works_in_progress/freebsd-10.0-amd64/scripts/bash.sh
@@ -3,7 +3,7 @@
 #bash is already installed
 
 #Change the default shell for the vagrant and root users to bash
-#Source: https://www.freebsd.org/doc/handbook/shells.html
+#Source: 4.9.1. Changing the Shell: https://www.freebsd.org/doc/handbook/shells.html
 
 chsh -s /usr/local/bin/bash vagrant
 chsh -s /usr/local/bin/bash root
