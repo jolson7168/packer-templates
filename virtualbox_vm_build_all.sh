6d7126350348758b3d0fd034bc6318727d7ae5be
Nick Chappell <nick@intronic.org>
2014-09-20 21:19:40 -0700

    Updated the JSON file names in the Virtualbox build-all script.

--- virtualbox_vm_build_all.sh
+++ virtualbox_vm_build_all.sh
@@ -2,21 +2,21 @@
 
 #Debian 7.6
 cd ./debian-7.6-amd64
-packer build --only=virtualbox-iso template.json
+packer build --only=virtualbox-iso debian-7.6-amd64.json
 
 #CentOS 6.5
 cd ../centos-6.5-amd64
-packer build --only=virtualbox-iso template.json
+packer build --only=virtualbox-iso centos-6.5-amd64.json
 
 #CentOS 7.0
 cd ../centos-7.0-amd64
-packer build --only=virtualbox-iso template.json
+packer build --only=virtualbox-iso centos-7.0-amd64
 
 #Ubuntu 12.04: this template is for an older version of Ubuntu, but 12.04 is a still-supported LTS release
 #so we still want to have fresh builds of it
 cd ../ubuntu-12.04-server-amd64
-packer build --only=virtualbox-iso template.json
+packer build --only=virtualbox-iso ubuntu-12.04-server-amd64.json
 
 #Ubuntu 14.04
 cd ../ubuntu-14.04-server-amd64
-packer build --only=virtualbox-iso template.json
+packer build --only=virtualbox-iso ubuntu-14.04-server-amd64.json
