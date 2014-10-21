5ce0646415c87cd3cfdbb5d10186ebe83d214f24
Nick Chappell <nick@intronic.org>
2014-10-20 23:55:50 -0700

    Add Debian 7.6 to the old VirtualBox build script.

--- old_virtualbox_vm_build_all.sh
+++ old_virtualbox_vm_build_all.sh
@@ -23,6 +23,10 @@ packer build --only=virtualbox-iso template.json
 cd ./debian-7.5-amd64
 packer build --only=virtualbox-iso template.json
 
+#Debian 7.6
+cd ./debian-7.6-amd64
+packer build --only=virtualbox-iso template.json
+
 #CentOS 6.4
 cd ../centos-6.4-amd64
 packer build --only=virtualbox template.json
