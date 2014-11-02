aece473575c7628864a6b765e9ba7005532fa1c0
Nick Chappell <nick@intronic.org>
2014-11-01 21:49:37 -0700

    Added a CentOS 6.6 template to works_in_progress.

---
+++ works_in_progress/centos-6.6-amd64/scripts/virtualbox.sh
@@ -0,0 +1,12 @@
+#Install kernel headers, GCC and some other things that we'll need to compile and install the guest additions:
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+
+# Installing the virtualbox guest additions
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+
+#Remove the guest additions ISO so it doesn't take up space in the finished base box:
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
---
+++ works_in_progress/centos-6.6-amd64/scripts/yum.sh
@@ -0,0 +1,22 @@
+#Yum and RPM package installations
+
+#Install EPEL:
+yum -y install epel-release
+
+#Install the CentOS Software Collections repo. More info available here:
+# https://access.redhat.com/site/documentation/en-US/Red_Hat_Software_Collections/1/html-single/1.0_Release_Notes/index.html
+
+yum -y install centos-release-SCL
+
+yum -y check-update
+
+yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget dkms nfs-utils
+
+#more packages
+yum -y install man nc vim nano tmux htop tree lsof curl wget screen telnet mailx unzip yum-utils rsync ncdu mosh ccze pv rlwrap
+
+#Some metrics gathering tools
+yum -y install apachetop atop dstat sysstat glances iotop smartmontools
+
+#Install ag
+rpm -Uvhi http://swiftsignal.com/packages/centos/6/x86_64/the-silver-searcher-0.14-1.el6.x86_64.rpm
