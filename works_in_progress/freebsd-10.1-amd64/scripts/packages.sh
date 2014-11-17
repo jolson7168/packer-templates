6ffed8bc0452575f085b2d106353a8a523645dcf
Nick Chappell <nick@intronic.org>
2014-11-16 19:43:39 -0800

    Create a FreeBSD 10.1 template in works_in_progress.

---
+++ works_in_progress/freebsd-10.1-amd64/scripts/packages.sh
@@ -0,0 +1,4 @@
+#Install various packages that are nice to have
+#Source: 5.4. Using pkg for Binary Package Management: https://www.freebsd.org/doc/en_US.ISO8859-1/books/handbook/pkgng-intro.html
+
+pkg install -y vim wget curl htop
